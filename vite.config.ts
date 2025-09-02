ef5bfc4cee78e97f53a553988afd87d5ad2a0d29
Eduarda <galvaovsco@gmail.com>
2025-09-02 15:35:50 -0300

    Atualiza config para deploy no Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/qr-card-humanas/",
+  base: "/",
   plugins: [
     react(),
-    mode === "development" &&
-      componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
